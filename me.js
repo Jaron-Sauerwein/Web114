14664cbb758b32304d9420d999f5c58144863532
Jaron-Sauerwein <pacerdude79@gmail.com>
2026-02-25 11:04:55 -0600

    Add files via upload

---
+++ me.js
@@ -0,0 +1,17 @@
+//Jaron All about me 2/25/2026
+
+let name = "Jaron";
+let favcolor = "blue";
+let hobby = "Giving tours at the Kansas City Automotive Museum";
+let food = "bacon cheeseburger";
+let animal = "capybara";
+let season = "fall";
+let restuarant = "Freebirds World Burrito";
+
+console.log("My name is: ", name);
+console.log("My favorite color is: ", favcolor);
+console.log("My favorite hobby is: ", hobby);
+console.log("My favorite food is: ", food);
+console.log("My favorite critter is: ", animal);
+console.log("My favorite season is: ", season);
+console.log("My favorie restaurant is: ", restuarant);
