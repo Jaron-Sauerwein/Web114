1e749d99d46575d4c14d8a6ca14b3901b13ec4c4
Jaron-Sauerwein <pacerdude79@gmail.com>
2026-02-25 11:46:02 -0600

    Add files via upload

---
+++ thenewme.js
@@ -0,0 +1,25 @@
+//Jaron All about me 2/25/2026
+
+let name = "Jaron";
+let favcolor = "blue";
+let hobby = "Giving tours at the Kansas City Automotive Museum";
+let food = "bacon cheeseburger";
+let animal = "capybara";
+let season = "fall";
+let restuarant = "Freebirds World Burrito";
+
+console.log("My name is: ", name);
+console.log("My favorite color is: "+ favcolor);
+console.log("My favorite hobby is: "+ hobby);
+console.log("My favorite food is: "+ food);
+console.log("My favorite critter is: "+ animal);
+console.log("My favorite season is: "+ season);
+console.log("My favorie restaurant is: "+ restuarant);
+console.log("         ");
+console.log("         ");
+console.log("My name is "+ name+ ".");
+console.log( "I am a fan of the color "+ favcolor+ ".");
+console.log(hobby+ " is my favorite activity once my work for the week is done.");
+console.log("I'd love to have a delicious "+ food+ " for lunch today.");
+console.log("My favorite animal is the "+ animal+ " which are often seen in Argentina during my favorite season, which is "+ season+ ".");
+console.log("My favorite restaurant, which I turly miss as it has left Kansas City, is "+ restuarant+ ".");
