0ae64a5fb0e4c3030a688bb274b8fdd24f2af352
Jaron-Sauerwein <pacerdude79@gmail.com>
2026-03-04 09:02:52 -0800

    Add files via upload

---
+++ Fortune/fortune.js
@@ -0,0 +1,7 @@
+let career = "Laundramat Supervisor";
+let salary = "$238,968 per year";
+let geolocation = "Valcau de Jos, Romania";
+let pname = "Nkiru Sohaila Yoshie Ye-won";
+let childnum = "14";
+
+console.log( " You will be a " + career + " in " + geolocation + " and married to " + pname + " with " + childnum + " kids with a salary of " + salary + ".")
